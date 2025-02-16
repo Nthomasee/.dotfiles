36b27a4d402625a98fb68d759a0f603e70db2f2e
Nthomase <niktynat82@protonmail.com>
2025-02-16 04:21:25 +0200

    .

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,30 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+
+#===================Aliases===================
+abbr -a -- ls 'eza'
+abbr -a -- lsa 'eza -a'
+abbr -a -- lsal 'eza -al'
+abbr -a -- v 'nvim'
+abbr -a -- osu '~/Downloads/osu.AppImage'
+#=============================================
+
+
+#====================Path=====================
+fish_add_path '/usr/local/texlive/2024/bin/x86_64-linux'
+#=============================================
+
+
+#==================Variables==================
+set -g fish_key_bindings fish_vi_key_bindings
+set -gx EDITOR nvim
+set -gx SDL_VIDEODRIVER wayland
+set fish_greeting ""
+#=============================================
+
+
+#===================Plugins===================
+starship init fish | source
+zoxide init fish | source
+#=============================================
+
 end
